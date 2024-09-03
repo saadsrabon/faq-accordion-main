8f5fd1b5560a49fce521b352c7998573061a9dc2
Saad Srabon <saadsrabon2@gmail.com>
2024-09-04 02:50:41 +0600

    Js Include done

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// first method with event bubble
-const faqBody = document.querySelector('.faq_body')
-let minus=true
-faqBody.addEventListener('click',(e)=>{
-if(minus){
-    e.target.src='./assets/images/icon-minus.svg'
-}else{
-      e.target.src='./assets/images/icon-plus.svg'
-}
-minus=!minus
-})
+const faqContainer = document.querySelector('.faq_body');
+
+// Event listener for all FAQ items
+faqContainer.addEventListener('click', (e) => {
+  // Check if the clicked element is the plus/minus icon or the question text
+  const target = e.target.closest('.faq_body_show_text');
+  if (target) {
+    // Toggle the hide class on the corresponding answer paragraph
+    const answer = target.nextElementSibling;
+    answer.classList.toggle('hide');
+
+    // Toggle the icon image source
+    const icon = target.querySelector('img');
+    if (icon.src.includes('icon-plus.svg')) {
+      icon.src = './assets/images/icon-minus.svg';
+    } else {
+      icon.src = './assets/images/icon-plus.svg';
+    }
+  }
+});
